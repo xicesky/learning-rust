a77fa146a4f7f8b05a2618464babed08186cae02
Markus Dangl <663399+xicesky@users.noreply.github.com>
2024-08-04 18:05:07 +0200

    chldsp: Check password via bcrypt.

--- chldsp/src/main.rs
+++ chldsp/src/main.rs
@@ -1,6 +1,30 @@
+use pwhash::bcrypt;
 use std::env::Args;
 use std::process::{exit, Command};
 use std::os::unix::process::CommandExt;
+
+static HASH: &str = "";
+
+fn check_and_exec(command: &String, args: Vec<String>) -> i32 {
+    match rpassword::prompt_password("Password: ") {
+        Err(_) => {
+            eprintln!("Failed to read password");
+            return 1;
+        },
+        Ok(password) => {
+            if !bcrypt::verify(password, HASH) {
+                eprintln!("Invalid password");
+                return 1;
+            }
+
+            let err = Command::new(command)
+                .args(&args)
+                .exec();
+            eprintln!("Failed to execute command: {}", err);
+            return 1;
+        },
+    }
+}
 
 fn run(mut args: Args) -> i32 {
     match args.nth(1) {
@@ -10,13 +34,10 @@ fn run(mut args: Args) -> i32 {
         },
         Some(ref command) => {
             let args: Vec<String> = args.collect();
+            
             // Print full command line
             // eprintln!("Command line: {} {:?}", command, args);
-            let err = Command::new(command)
-                .args(&args)
-                .exec();
-            eprintln!("Failed to execute command: {}", err);
-            return 1;
+            return check_and_exec(command, args);
         },
     }
 }
